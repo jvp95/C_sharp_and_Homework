b308d922ece20a85a3c5202a3b0fd2ad759d7955
Evgenii <paholkov9590@gmail.com>
2023-05-18 17:28:46 +0300

    add task0 прога  квадрат числа

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine()!);
-int result = number * number;
+﻿Console.WriteLine("Введите число 1");
+Console.WriteLine("Введите число 2");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+int result = number1 = number2 * number2;
 Console.WriteLine("Квадрат числа равен = " + result);
