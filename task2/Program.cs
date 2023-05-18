e16c4dfe8f8d55c3e9c6c56bfff8907cdfdb330d
Evgenii <paholkov9590@gmail.com>
2023-05-18 18:12:04 +0300

    add task2 Задача про Среду и Пятницу

---
+++ task2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.WriteLine("Введите день недели");
+int weekDay = int.Parse(Console.ReadLine()!);
+if(weekDay==1)
+{
+    Console.WriteLine("Пн");
+}
+else if(weekDay==2)
+{
+    Console.WriteLine("Вт");
+}
+else if(weekDay==3)
+{
+    Console.WriteLine("Среда");
+}
+else if(weekDay==4)
+{
+    Console.WriteLine("Чт");
+}
+else if(weekDay==5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(weekDay==6)
+{
+    Console.WriteLine("Сб");
+}
+else if(weekDay==7)
+{
+    Console.WriteLine("Вс");
+}
+else
+{
+    Console.WriteLine("Wrong number!");
+}
