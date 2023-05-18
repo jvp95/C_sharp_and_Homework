df44c5815895d912e2be33b48b2c342bf84556a6
Evgenii <paholkov9590@gmail.com>
2023-05-18 17:26:51 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Является ли число 1 квадратом числа 2");
+Console.WriteLine("Введите число 1");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2");
+int number2 = int.Parse(Console.ReadLine()!);
+int result = number2 * number2;
+if (result == number1)
+{
+Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}    
