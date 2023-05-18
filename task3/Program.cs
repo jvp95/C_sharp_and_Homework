b262be6e43fe5eaa38581cb13ac9ad3508a7afcd
Evgenii <paholkov9590@gmail.com>
2023-05-18 18:51:20 +0300

    add task3 Задача про целые числа

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = number1 * -1;
+int temp;
+if (number1 < 0)
+{
+    temp = number1;
+    number1 = number2;
+    number2 = temp;
+}
+while (number2 <= number1)
+{
+    Console.WriteLine(number2 + "");
+    number2 += 1;
+}
