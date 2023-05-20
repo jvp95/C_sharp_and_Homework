4a66d4bfd6d28af8f4732d3321e4081d9edc6da8
Evgenii <paholkov9590@gmail.com>
2023-05-20 14:31:56 +0300

    add Homework0 Задача 2

--- Homework0/Program.cs
+++ Homework0/Program.cs
@@ -3,4 +3,24 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число 1");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2");
+int number2 = int.Parse(Console.ReadLine()!);
+int result = number1;
+if (number1<number2)
+{
+    Console.WriteLine("max = " + number2);
+}
+else 
+{
+    Console.WriteLine("max = " + result);
+}
+if (number1>number2)
+{
+    Console.WriteLine("min = " + number2);
+}
+else 
+{
+    Console.WriteLine("min = " + result);
+}
