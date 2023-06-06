cf8a07c0cd0bb710a0987ae872f6597dc93e4f6c
Evgenii <paholkov9590@gmail.com>
2023-06-06 21:01:12 +0300

    add Homework01 task 13

---
+++ Homework01/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt(string msg)
+{
+
+    Console.WriteLine((msg));
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+
+int fnumber(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool check(int number)
+{
+    if (number < 100)
+        return false;
+    else return true;
+}
+
+if (check(number) != true)
+    Console.WriteLine("Третьей цифры нет");
+else
+    
+    Console.WriteLine(fnumber(number));
+
+
+
+
