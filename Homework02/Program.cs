e6f82d744f9cf3ec0cf506801cdeded350b9919b
Evgenii <paholkov9590@gmail.com>
2023-06-06 21:20:12 +0300

    add Homework02 task 15

---
+++ Homework02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int dayNumber = ReadInt("Введите число от 1 до 7:");
+Console.WriteLine(WorkHoliday(dayNumber));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write(a + " - Выходной");
+        }
+        else
+        {
+            Console.Write(a + " - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели некорректный");
+    }
+        return " день.";
+}
