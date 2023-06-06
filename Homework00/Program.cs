6feaf1dcdbf787dc09a5d9d003f3c8a8b3d87167
Evgenii <paholkov9590@gmail.com>
2023-06-06 20:01:46 +0300

    add Homework00 task 10

---
+++ Homework00/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число:");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+if (Length == 3) 
+{
+	int result = ( Value/10 ) % 10;
+	Console.WriteLine("Вторая цифра " + result);
+} else {
+	Console.WriteLine("Не трехзначное число");
+}
--- task00/Program.cs
+++ task00/Program.cs
@@ -18,16 +18,16 @@ int FindSecondDigit(int num)
     return secondNumber;
 }
 
-void FindLargestDigit(int num,int firstNumber,int secondNumber)
+void FindLargestDigit(int num, int firstNumber, int secondNumber)
 {
     Console.WriteLine("Рандомное число: " + num);
     Console.WriteLine(firstNumber);
     Console.WriteLine(secondNumber);
-    if (firstNumber>secondNumber)
+    if (firstNumber > secondNumber)
     {
         Console.WriteLine("Первая цифра больше второй");
     }
-    else if(secondNumber>firstNumber)
+    else if (secondNumber > firstNumber)
     {
         Console.WriteLine("Вторая цифра больше второй");
     }
@@ -36,20 +36,20 @@ void FindLargestDigit(int num,int firstNumber,int secondNumber)
         Console.WriteLine("Цифры равны");
     }
 }
-int number = new Random().Next(10,100);
+int number = new Random().Next(10, 100);
 int firstDigit = FindFirstDigit(number);
 int secondDigit = FindSecondDigit(number);
-FindLargestDigit(number,firstDigit,secondDigit);
+FindLargestDigit(number, firstDigit, secondDigit);
 
 int number1 = new Random().Next(100);
 int firstDigit1 = FindFirstDigit(number1);
 int secondDigit1 = FindSecondDigit(number1);
-FindLargestDigit(number1,firstDigit1,secondDigit1);
+FindLargestDigit(number1, firstDigit1, secondDigit1);
 
-int number2 = new Random().Next(-80,10);
+int number2 = new Random().Next(-80, 10);
 int firstDigit2 = FindFirstDigit(number2);
 int secondDigit2 = FindSecondDigit(number2);
-FindLargestDigit(number2,firstDigit2,secondDigit2);
+FindLargestDigit(number2, firstDigit2, secondDigit2);
 
 
 // FindLargestDigit(number);
