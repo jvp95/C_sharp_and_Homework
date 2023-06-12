cdc283fab18725c361f356d26d8904ca7e9925eb
Evgenii <paholkov9590@gmail.com>
2023-06-12 17:58:19 +0300

    add task001 Задача 18 Нахождение четверти

---
+++ task001/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 18: Напишите программу, которая по 
+// заданному номеру четверти, показывает диапазон 
+// возможных координат точек в этой четверти (x и y).
+
+
+int num = Prompt("Введите номер четверти:");
+
+int Prompt(string message)
+{
+Console.Write(message);
+int num = int.Parse(Console.ReadLine()!);
+return num;
+}
+ShowQuater(num);
+
+void FindCoordinate(int num)
+{
+    if (num == 1)
+    {
+        Console.WriteLine("I четверть X>0  Y>0");
+    }
+    if (num == 2)
+    {
+        Console.WriteLine("II четверть X<0  Y>0");
+    }
+    if (num == 3)
+    {
+        Console.WriteLine("III четверть X<0 Y<0");
+    }
+    if (num == 4)
+    {
+        Console.WriteLine("IV четверть X>0  Y<0}");
+    }
+}
+void ShowQuater(int num)
+{
+if (num < 1 || num > 4)
+{
+    Console.WriteLine("Введена неправильная четверть");
+}
+else
+{
+    FindCoordinate(num);
+}
+}
