672234900bb2b205d2c6696a957a36f6d3a2665b
Evgenii <paholkov9590@gmail.com>
2023-06-12 16:25:44 +0300

    add task000 Координаты X и Y

---
+++ task000/Program.cs
@@ -0,0 +1,35 @@
+﻿// 17. Напишите программу, которая принимает на вход 
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт 
+// номер четверти плоскости, в которой находится эта 
+// точка.
+
+int x1 = Prompt("Введите координату X: ");
+int y1 = Prompt("Введите координату Y: ");
+FindCoordinate(x1, y1);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void FindCoordinate(int x, int y)
+{
+    if(x>0 && y>0)
+    {
+        Console.WriteLine("I четверть");
+    }
+    if(x<0 && y>0)
+    {
+        Console.WriteLine("II четверть");
+    }
+    if(x<0 && y<0)
+    {
+        Console.WriteLine("III четверть");
+    }
+    if(x>0 && y<0)
+    {
+        Console.WriteLine("IV четверть");
+    }
+}
